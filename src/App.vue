f4858cce361ceabb111cee4369553ef919bab854
David Ibanez <dibanezbal@gmail.com>
2021-02-09 21:20:23 +0100

    Starting the tutorial for vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Personas</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <tabla-personas :personas="personas" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import TablaPersonas from '@/components/TablaPersonas.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      TablaPersonas,
+    },
+    data() {
+      return {
+        personas: [
+          {
+            id: 1,
+            nombre: 'Jon',
+            apellido: 'Nieve',
+            email: 'jon@email.com',
+          },
+          {
+            id: 2,
+            nombre: 'Tyrion',
+            apellido: 'Lannister',
+            email: 'tyrion@email.com',
+          },
+          {
+            id: 3,
+            nombre: 'Daenerys',
+            apellido: 'Targaryen',
+            email: 'daenerys@email.com',
+          },
+        ],
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
 </style>
